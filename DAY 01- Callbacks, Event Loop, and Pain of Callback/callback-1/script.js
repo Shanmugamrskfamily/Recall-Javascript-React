2fa6a6361196e2fb1ffe1732ffb7248cfadc76ff
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2023-09-01 21:19:55 +0530

    completed

---
+++ DAY 01- Callbacks, Event Loop, and Pain of Callback/callback-1/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let step = 1;
+    let resultElement = document.getElementById("result");
+
+    function fetchData(callback) {
+      setTimeout(function () {
+        const data = { message: "Data fetched successfully" };
+        callback(data);
+      }, 1000);
+    }
+
+    function processData(data, callback) {
+      setTimeout(function () {
+        const processedData = data.message.toUpperCase();
+        callback(processedData);
+      }, 1000);
+    }
+
+    function performAdditionalOperation(processedData, callback) {
+      setTimeout(function () {
+        const result = `${processedData}`;
+        callback(result);
+      }, 1000);
+    }
+
+    if (step === 1) {
+      fetchData(function (data) {
+        resultElement.innerText = "Step 1: Data fetched.";
+        step = 2;
+        processData(data, function (processedData) {
+          resultElement.innerText = "Step 2: Data processed.";
+          step = 3;
+          performAdditionalOperation(processedData, function (result) {
+            resultElement.innerText =
+              "Step 3: Additional operation completed. " + result;
+          });
+        });
+      });
+    }
+  });
