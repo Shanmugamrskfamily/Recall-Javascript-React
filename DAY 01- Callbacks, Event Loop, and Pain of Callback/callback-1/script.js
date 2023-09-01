09c5d06d5691330c45462f7b413c481942833325
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2023-09-01 21:47:43 +0530

    UI update

--- DAY 01- Callbacks, Event Loop, and Pain of Callback/callback-1/script.js
+++ DAY 01- Callbacks, Event Loop, and Pain of Callback/callback-1/script.js
@@ -1,41 +1,39 @@
-document.addEventListener("DOMContentLoaded", 
-function () {
-    let step = 1;
-    let resultElement = document.getElementById("result");
-
+document.addEventListener("DOMContentLoaded", function () {
+    const resultElement = document.getElementById("result");
+  
     function fetchData(callback) {
       setTimeout(function () {
         const data = { message: "Data fetched successfully" };
         callback(data);
-      }, 1000);
+      }, 2000);
     }
-
+  
     function processData(data, callback) {
       setTimeout(function () {
         const processedData = data.message.toUpperCase();
         callback(processedData);
-      }, 1000);
+      }, 2000);
     }
-
-    function performAdditionalOperation(processedData, callback) {
+  
+    function performFinalOperation(processedData, callback) {
       setTimeout(function () {
         const result = `${processedData}`;
         callback(result);
-      }, 1000);
+      }, 2000);
     }
-    
-    if (step === 1) {
-      fetchData(function (data) {
-        resultElement.innerText = "Step 1: Data fetched.";
-        step = 2;
-        processData(data, function (processedData) {
-          resultElement.innerText = "Step 2: Data processed.";
-          step = 3;
-          performAdditionalOperation(processedData, function (result) {
-            resultElement.innerText =
-              "Step 3: Additional operation completed. " + result;
-          });
+  
+    function displayResult(result) {
+      resultElement.innerText = result;
+    }
+  
+    fetchData(function (data) {
+      displayResult("Step 1: Data fetched.");
+      processData(data, function (processedData) {
+        displayResult("Step 2: Data processed.");
+        performFinalOperation(processedData, function (result) {
+          displayResult(`Step 3: Final operation completed. ${result}`);
         });
       });
-    }
+    });
   });
+  
