9d1b4c15dcbd574ce1df4ed303ac974a7eda8593
Shanmugamrskfamily <shanmugamrskfamily@email.com>
2023-12-20 15:37:58 +0530

    on going

---
+++ Day 01_20-12-23/dataTypes.js
@@ -0,0 +1,13 @@
+const person={name:"SHANMUGAM",age:27,address:"Chennai", isStudent:true};
+console.log(`Name: ${person.name}`);
+console.log(`Age: ${person.age}`);
+console.log(`Address: ${person.address}`);
+console.log(`Is Student: ${person.isStudent}`)
+const personData=document.getElementById("root");
+personData.innerHTML=`<div style="text-align: center">
+<h1>Person Details</h1></br>
+<p ><b>Name:</b> ${person.name}</br>
+<b>Age:</b> ${person.age}</br>
+<b>Is Student:</b> ${person.isStudent}</br>
+<b>Address:</b> ${person.address}</p>
+</div>`
---
+++ Day 01_20-12-23/variableManipulations.js
@@ -0,0 +1,15 @@
+let num1=10,num2=5;
+let addition=num1+num2;
+let multiplication= num1* num2;
+console.log(`Addition: ${addition}`);
+console.log(`Multiplication: ${multiplication}`);
+
+const str1='Hello! ';
+const str2='SHANMUGAM R';
+const msg=`${str1+str2}`;
+console.log(`msg: ${msg}`);
+
+const isActive=true;
+const isLoggedin=false;
+const combinedBoolean=isActive&&isLoggedin;
+console.log(`CombinedBoolean: ${combinedBoolean}`);
